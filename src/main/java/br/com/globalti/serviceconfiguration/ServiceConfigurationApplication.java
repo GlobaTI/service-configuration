b4872175ecc425b30078aa17ac4a75992588896b
Kleber Nunes <kleber_nunes99@hotmail.com>
2018-09-22 21:55:16 -0300

    initial

---
+++ src/main/java/br/com/globalti/serviceconfiguration/ServiceConfigurationApplication.java
@@ -0,0 +1,12 @@
+package br.com.globalti.serviceconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConfigurationApplication.class, args);
+	}
+}
